f2d18aae98bc048536da143bae015f5e85f7cc70
Rony Rojas <rony.rojas@tcisaperu.onmicrosoft.com>
2023-06-14 13:15:12 -0500

    Primera Version

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Conexión a la base de datos MongoDB
+mongoose.connect('mongodb+srv://rrojamir:KRYYiiKLOi71YWwq@4emstudios.0ejggy5.mongodb.net/botwsppru', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conectado a la base de datos MongoDB');
+});
+
+// Definición del esquema y el modelo
+const itemSchema = new mongoose.Schema({
+  producto: String,
+  precio: Number,
+  descripcion: String,
+});
+const Item = mongoose.model('producto', itemSchema);
+// Ruta para obtener todos los registros
+app.get('/', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Ocurrió un error al obtener los registros' });
+  }
+});
+
+// Ruta para crear un nuevo registro
+app.post('/', async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.status(201).json(newItem);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Ocurrió un error al crear el registro' });
+  }
+});
+
+// Ruta para obtener un registro por su ID
+app.get('/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+      return res.status(404).json({ error: 'El registro no fue encontrado' });
+    }
+    res.json(item);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Ocurrió un error al obtener el registro' });
+  }
+});
+
+// Ruta para actualizar un registro existente
+app.put('/:id', async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedItem) {
+      return res.status(404).json({ error: 'El registro no fue encontrado' });
+    }
+    res.json(updatedItem);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Ocurrió un error al actualizar el registro' });
+  }
+});
+
+// Ruta para eliminar un registro existente
+app.delete('/:id', async (req, res) => {
+  try {
+    const deletedItem = await Item.findByIdAndDelete(req.params.id);
+    if (!deletedItem) {
+      return res.status(404).json({ error: 'El registro no fue encontrado' });
+    }
+    res.json({ message: 'Registro eliminado correctamente' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Ocurrió un error al eliminar el registro' });
+  }
+});
+
+// Iniciar el servidor
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Servidor web en funcionamiento en http://localhost:${port}`);
+});
---
+++ script.js
@@ -0,0 +1,129 @@
+// Variables globales
+const form = document.getElementById('form');
+const itemsContainer = document.getElementById('items-container');
+
+// Obtener todos los registros al cargar la página
+window.addEventListener('DOMContentLoaded', getItems);
+
+// Enviar el formulario para crear o actualizar un registro
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const id = form.dataset.itemId;
+  const producto = document.getElementById('producto').value;
+  const descripcion = document.getElementById('descripcion').value;
+  const precio = document.getElementById('precio').value;
+
+  if (id) {
+    // Actualizar un registro existente
+    updateItem(id, producto, descripcion, precio);
+  } else {
+    // Crear un nuevo registro
+    createItem(producto, descripcion, precio);
+  }
+
+  form.reset();
+});
+
+// Crear un nuevo registro
+function createItem(producto, descripcion, precio) {
+  fetch('http://localhost:3001/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ producto, descripcion, precio }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      displayItem(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Obtener todos los registros
+function getItems() {
+  fetch('http://localhost:3001/')
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((item) => {
+        displayItem(item);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Mostrar un registro en la interfaz
+function displayItem(item) {
+  const itemElement = document.createElement('div');
+  itemElement.classList.add('item');
+  itemElement.innerHTML = `
+    <div>
+      <h3 style="text-transform: uppercase;">${item.producto}</h3>
+      <p>${item.descripcion}</p>
+      <p>Precio: S/ ${item.precio}</p>
+    </div>
+    <div>
+      <button class="btn btn-secondary" onclick="editItem('${item._id}')">Editar</button>
+      <button class="btn btn-danger" onclick="deleteItem('${item._id}')">Eliminar</button>
+    </div>
+  `;
+
+  itemsContainer.appendChild(itemElement);
+}
+
+// Editar un registro
+function editItem(id) {
+  fetch(`http://localhost:3001/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      form.dataset.itemId = data._id;
+      document.getElementById('producto').value = data.producto;
+      document.getElementById('descripcion').value = data.descripcion;
+      document.getElementById('precio').value = data.precio;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Actualizar un registro existente
+function updateItem(id, producto, descripcion, precio) {
+  fetch(`http://localhost:3001/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ producto, descripcion, precio }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const updatedItem = document.querySelector(`[data-item-id="${data._id}"]`);
+      updatedItem.querySelector('h3').textContent = data.producto;
+      updatedItem.querySelector('p:first-child').textContent = data.descripcion;
+      updatedItem.querySelector('p:last-child').textContent = `Precio: ${data.precio}`;
+      delete form.dataset.itemId;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Eliminar un registro
+function deleteItem(id) {
+  fetch(`http://localhost:3001/${id}`, {
+    method: 'DELETE',
+  })
+    .then(() => {
+      const deletedItem = document.querySelector(`[data-item-id="${id}"]`);
+      deletedItem.remove();
+    })
+    .catch((error) => {
+      displayItem(data);
+      console.error(error);
+    });
+}
